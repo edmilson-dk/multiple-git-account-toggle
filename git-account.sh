90d7d4e75a8f56715db761b57257376988569cb2
edmilson-dk <droidvidaboa@gmail.com>
2021-02-18 11:22:45 -0300

    feat: finished creating the script to switch between my github accounts on the terminal

--- git-account.sh
+++ git-account.sh
@@ -14,18 +14,27 @@ read ACCOUNT
 
 case $ACCOUNT in
 1)
-  USER_NAME="DKSecurity99"
-  USER_EMAIL="dksorteio@gmail.com"
+  USER_NAME="your_username"
+  USER_EMAIL="your_email"
+  SETED=true
   ;;
 2)
-  USER_NAME="edmilson-dk"
-  USER_EMAIL="droidvidaboa@gmail.com"
+  USER_NAME="your_username"
+  USER_EMAIL="your_email"
+  SETED=true
   ;;
 *) 
+  SETED=false
   echo '\n!Invalid option\n'
 esac
 
-git config --global user.name $USER_NAME
-git config --global user.email $USER_EMAIL
+if [ $SETED != false ]
+then
+  git config --global user.name $USER_NAME
+  git config --global user.email $USER_EMAIL
+  
+  echo "Global account changed to: "$USER_NAME "("$USER_EMAIL")"
+else
+  echo "!There was no exchange of github accounts."
+fi
 
-echo "Global account changed to: "$USER_NAME "("$USER_EMAIL")"
